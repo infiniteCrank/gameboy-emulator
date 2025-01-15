11da6433aa0a26222926925483f9f6e5ce1badef
JulianWilkison-Duran <clockworksyler@gmail.com>
2025-01-15 02:45:14 -0500

    enhancements

--- cpu/cpu.go
+++ cpu/cpu.go
@@ -54,27 +54,224 @@ func (cpu *CPU) Execute(memory Memory) {
 	switch opcode {
 	case 0x00: // NOP
 		cpu.Cycles += 4
-	case 0x01: // LDI BC, d16
+
+	// LD instructions
+	case 0x01: // LD BC, d16
 		cpu.B = memory.Read(cpu.PC)
 		cpu.C = memory.Read(cpu.PC + 1)
 		cpu.PC += 2
 		cpu.Cycles += 12
-	case 0x02: // LDI (BC), A
+	case 0x02: // LD (BC), A
 		addr := (uint16(cpu.B) << 8) | uint16(cpu.C)
 		memory.Write(addr, cpu.A)
 		cpu.Cycles += 8
-	case 0x80: // ADD A, A
+	case 0x0A: // LD A, (BC)
+		cpu.A = memory.Read((uint16(cpu.B) << 8) | uint16(cpu.C))
+		cpu.Cycles += 8
+	case 0x3E: // LD A, d8
+		cpu.A = memory.Read(cpu.PC)
+		cpu.PC++
+		cpu.Cycles += 8
+	case 0x32: // LD (HL-), A
+		addr := (uint16(cpu.H) << 8) | uint16(cpu.L)
+		memory.Write(addr, cpu.A)
+		cpu.L--
+		cpu.Cycles += 8
+	case 0x36: // LD (HL), d8
+		addr := (uint16(cpu.H) << 8) | uint16(cpu.L)
+		memory.Write(addr, memory.Read(cpu.PC))
+		cpu.PC++
+		cpu.Cycles += 12
+
+	// ADD instructions
+	case 0xC6: // ADD A, d8
+		value := memory.Read(cpu.PC)
+		cpu.PC++
+		cpu.Add(value)
+		cpu.Cycles += 8
+	case 0x87: // ADD A, A
 		cpu.Add(cpu.A)
 		cpu.Cycles += 4
-	case 0x90: // SUB A, A
-		cpu.Sub(cpu.A)
+	case 0x80: // ADD A, B
+		cpu.Add(cpu.B)
 		cpu.Cycles += 4
+	case 0x82: // ADD A, D
+		cpu.Add(cpu.D)
+		cpu.Cycles += 4
+
+	// SUB instructions
+	case 0xD6: // SUB d8
+		value := memory.Read(cpu.PC)
+		cpu.PC++
+		cpu.Sub(value)
+		cpu.Cycles += 8
+
+	// INC instructions
+	case 0x04: // INC B
+		cpu.B++
+		cpu.SetZeroFlagIfNeeded(cpu.B)
+		cpu.Cycles += 4
+	case 0x0C: // INC C
+		cpu.C++
+		cpu.SetZeroFlagIfNeeded(cpu.C)
+		cpu.Cycles += 4
+	case 0x14: // INC D
+		cpu.D++
+		cpu.SetZeroFlagIfNeeded(cpu.D)
+		cpu.Cycles += 4
+	case 0x1C: // INC E
+		cpu.E++
+		cpu.SetZeroFlagIfNeeded(cpu.E)
+		cpu.Cycles += 4
+	case 0x24: // INC H
+		cpu.H++
+		cpu.SetZeroFlagIfNeeded(cpu.H)
+		cpu.Cycles += 4
+	case 0x2C: // INC L
+		cpu.L++
+		cpu.SetZeroFlagIfNeeded(cpu.L)
+		cpu.Cycles += 4
+	case 0x3C: // INC A
+		cpu.A++
+		cpu.SetZeroFlagIfNeeded(cpu.A)
+		cpu.Cycles += 4
+
+	// DEC instructions
+	case 0x05: // DEC B
+		cpu.B--
+		cpu.SetZeroFlagIfNeeded(cpu.B)
+		cpu.Cycles += 4
+	case 0x0D: // DEC C
+		cpu.C--
+		cpu.SetZeroFlagIfNeeded(cpu.C)
+		cpu.Cycles += 4
+	case 0x15: // DEC D
+		cpu.D--
+		cpu.SetZeroFlagIfNeeded(cpu.D)
+		cpu.Cycles += 4
+	case 0x1D: // DEC E
+		cpu.E--
+		cpu.SetZeroFlagIfNeeded(cpu.E)
+		cpu.Cycles += 4
+	case 0x25: // DEC H
+		cpu.H--
+		cpu.SetZeroFlagIfNeeded(cpu.H)
+		cpu.Cycles += 4
+	case 0x2D: // DEC L
+		cpu.L--
+		cpu.SetZeroFlagIfNeeded(cpu.L)
+		cpu.Cycles += 4
+	case 0x3D: // DEC A
+		cpu.A--
+		cpu.SetZeroFlagIfNeeded(cpu.A)
+		cpu.Cycles += 4
+
+	// XOR instructions
+	case 0xA8: // XOR B
+		cpu.A ^= cpu.B
+		cpu.ClearCarryFlag()
+		cpu.SetZeroFlagIfNeeded(cpu.A)
+		cpu.Cycles += 4
+	case 0xA9: // XOR C
+		cpu.A ^= cpu.C
+		cpu.ClearCarryFlag()
+		cpu.SetZeroFlagIfNeeded(cpu.A)
+		cpu.Cycles += 4
+	case 0xAA: // XOR D
+		cpu.A ^= cpu.D
+		cpu.ClearCarryFlag()
+		cpu.SetZeroFlagIfNeeded(cpu.A)
+		cpu.Cycles += 4
+	case 0xAB: // XOR E
+		cpu.A ^= cpu.E
+		cpu.ClearCarryFlag()
+		cpu.SetZeroFlagIfNeeded(cpu.A)
+		cpu.Cycles += 4
+	case 0xAC: // XOR H
+		cpu.A ^= cpu.H
+		cpu.ClearCarryFlag()
+		cpu.SetZeroFlagIfNeeded(cpu.A)
+		cpu.Cycles += 4
+	case 0xAD: // XOR L
+		cpu.A ^= cpu.L
+		cpu.ClearCarryFlag()
+		cpu.SetZeroFlagIfNeeded(cpu.A)
+		cpu.Cycles += 4
+	case 0xAE: // XOR (HL)
+		cpu.A ^= memory.Read((uint16(cpu.H) << 8) | uint16(cpu.L))
+		cpu.ClearCarryFlag()
+		cpu.SetZeroFlagIfNeeded(cpu.A)
+		cpu.Cycles += 8
+
+	// OR instructions
+	case 0xB0: // OR B
+		cpu.A |= cpu.B
+		cpu.ClearCarryFlag()
+		cpu.SetZeroFlagIfNeeded(cpu.A)
+		cpu.Cycles += 4
+	case 0xB1: // OR C
+		cpu.A |= cpu.C
+		cpu.ClearCarryFlag()
+		cpu.SetZeroFlagIfNeeded(cpu.A)
+		cpu.Cycles += 4
+	case 0xB2: // OR D
+		cpu.A |= cpu.D
+		cpu.ClearCarryFlag()
+		cpu.SetZeroFlagIfNeeded(cpu.A)
+		cpu.Cycles += 4
+	case 0xB3: // OR E
+		cpu.A |= cpu.E
+		cpu.ClearCarryFlag()
+		cpu.SetZeroFlagIfNeeded(cpu.A)
+		cpu.Cycles += 4
+	case 0xB4: // OR H
+		cpu.A |= cpu.H
+		cpu.ClearCarryFlag()
+		cpu.SetZeroFlagIfNeeded(cpu.A)
+		cpu.Cycles += 4
+	case 0xB5: // OR L
+		cpu.A |= cpu.L
+		cpu.ClearCarryFlag()
+		cpu.SetZeroFlagIfNeeded(cpu.A)
+		cpu.Cycles += 4
+	case 0xB6: // OR (HL)
+		cpu.A |= memory.Read((uint16(cpu.H) << 8) | uint16(cpu.L))
+		cpu.ClearCarryFlag()
+		cpu.SetZeroFlagIfNeeded(cpu.A)
+		cpu.Cycles += 8
+
+	// Control Flow Instructions
 	case 0xC3: // JP a16
-		addr := uint16(memory.Read(cpu.PC)) | uint16(memory.Read(cpu.PC+1))<<8
+		addr := uint16(memory.Read(cpu.PC)) | (uint16(memory.Read(cpu.PC+1)) << 8)
 		cpu.PC = addr
+		cpu.Cycles += 16
+
+	case 0x18: // JR r8
+		offset := int8(memory.Read(cpu.PC))
+		cpu.PC += uint16(offset) + 1
 		cpu.Cycles += 12
+
+	case 0xCD: // CALL a16
+		addr := uint16(memory.Read(cpu.PC)) | (uint16(memory.Read(cpu.PC+1)) << 8)
+		cpu.Push(cpu.PC, memory) // Push current PC to stack
+		cpu.PC = addr
+		cpu.Cycles += 24
+
+	case 0xC9: // RET
+		cpu.PC = cpu.Pop(memory) // Pop from stack to PC
+		cpu.Cycles += 16
+
 	default:
 		fmt.Printf("Unknown opcode: %02X at PC: %04X\n", opcode, cpu.PC-1)
+	}
+}
+
+// SetZeroFlagIfNeeded sets the zero flag if the value is zero
+func (cpu *CPU) SetZeroFlagIfNeeded(value byte) {
+	if value == 0 {
+		cpu.SetZeroFlag()
+	} else {
+		cpu.ClearZeroFlag()
 	}
 }
 
@@ -104,7 +301,7 @@ func (cpu *CPU) Add(value byte) {
 		cpu.ClearZeroFlag()
 	}
 	if result < cpu.A {
-		cpu.SetCarryFlag()
+		cpu.SetCarryFlag() // Carry flag if there is an overflow
 	} else {
 		cpu.ClearCarryFlag()
 	}
@@ -153,7 +350,15 @@ func (m *SimpleMemory) Write(addr uint16, value byte) {
 	m.data[addr] = value
 }
 
-// Example of how to use the CPU and memory in a main function
+// Convert boolean to int (0 or 1)
+func btoi(b bool) int {
+	if b {
+		return 1
+	}
+	return 0
+}
+
+// Main function to demonstrate CPU execution
 func main() {
 	mem := &SimpleMemory{}
 	cpu := NewCPU()
@@ -164,20 +369,21 @@ func main() {
 	mem.Write(0x0102, 0x12) // High byte
 	mem.Write(0x0103, 0x02) // LDI (BC), A
 	mem.Write(0x0104, 0x80) // ADD A, A
-	mem.Write(0x0105, 0x90) // SUB A, A
-	mem.Write(0x0106, 0xC3) // JP a16
-	mem.Write(0x0107, 0x00) // Address low byte for JP
-	mem.Write(0x0108, 0x10) // Address high byte for JP
+	mem.Write(0x0105, 0x3E) // LD A, d8
+	mem.Write(0x0106, 0x0A) // Load 10 into A
+	mem.Write(0x0107, 0xC6) // ADD A, d8
+	mem.Write(0x0108, 0x02) // ADD A, 2
+	mem.Write(0x0109, 0xC9) // RET
 
 	// Set initial values
 	cpu.A = 5        // Set Accumulator A to 5
 	cpu.Execute(mem) // Execute LDI BC, d16
 	cpu.Execute(mem) // Execute LDI (BC), A
 	cpu.Execute(mem) // Execute ADD A, A
-	cpu.Execute(mem) // Execute SUB A, A
-	cpu.Execute(mem) // Execute JP to address 0x1000 (just an example)
+	cpu.Execute(mem) // Execute LD A, d8
+	cpu.Execute(mem) // Execute ADD A, d8
 
-	// Check the results
+	// Print CPU Registers and Flags
 	fmt.Printf("A: %d (0x%02X)\n", cpu.A, cpu.A)
 	fmt.Printf("B: %d (0x%02X)\n", cpu.B, cpu.B)
 	fmt.Printf("C: %d (0x%02X)\n", cpu.C, cpu.C)
@@ -188,17 +394,9 @@ func main() {
 	fmt.Printf("SP: %04X\n", cpu.SP)
 	fmt.Printf("PC: %04X\n", cpu.PC)
 	fmt.Printf("F: %02X (Flags: Z: %d, N: %d, H: %d, C: %d)\n", cpu.F,
-		btoi(cpu.F&FlagZ != 0), // Convert bool to int
+		btoi(cpu.F&FlagZ != 0), // Convert bool to int (0 or 1)
 		btoi(cpu.F&FlagN != 0),
 		btoi(cpu.F&FlagH != 0),
 		btoi(cpu.F&FlagC != 0),
 	)
-}
-
-// Convert boolean to int (0 or 1)
-func btoi(b bool) int {
-	if b {
-		return 1
-	}
-	return 0
 }
